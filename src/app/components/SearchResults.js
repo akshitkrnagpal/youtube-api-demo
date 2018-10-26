c45476ea0780fd8bf7bafea5303939fdf27644c0
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-10-26 19:01:15 +0530

    Refactored

--- src/app/components/SearchResults.js
+++ src/app/components/SearchResults.js
@@ -24,17 +24,25 @@ type Props = {
 
 class SearchResults extends Component<Props> {
     render() {
+
+        // Return Loading.
         if (this.props._loading) {
             return <PlaceholderInfo icon = 'spinner'/>
         }
+
+        // Return Error Message.
         if (this.props._error) {
             const error = this.props._error;
             const message = error.data || error.response.data.error.message;
             return <PlaceholderInfo icon = 'exclamation' text = { message || 'Some Error Occured' } />
         }
+
+        // Return PLaceholder for Search.
         if (this.props._items.length <= 0) {
             return <PlaceholderInfo icon = 'search' text = 'Search for videos' />
         }
+
+        // Return Search Results.
         return (
             <Row>
                 <div className = 'clearfix w-100 py-4'>
@@ -51,22 +59,25 @@ class SearchResults extends Component<Props> {
     }
 
     _renderItems() {
+        // Don't consider Items other than Videos (like Channels).
         const items = this.props._items.filter(item => item.id.kind === 'youtube#video');
-        switch(this.props._sortBy) {
-            case 'title':
-                if ( this.props._order === 'asc' ) {
-                    items.sort((A, B) => A.snippet.title.localeCompare(B.snippet.title) );
-                } else {
-                    items.sort((A, B) => B.snippet.title.localeCompare(A.snippet.title) );
-                }
-                break;
-            case 'publishedDate':
-                if ( this.props._order === 'asc' ) {
-                    items.sort((A, B) => moment.utc(A.snippet.publishedAt).diff(moment.utc(B.snippet.publishedAt)));
-                } else {
-                    items.sort((A, B) => moment.utc(B.snippet.publishedAt).diff(moment.utc(A.snippet.publishedAt)));
-                }
-                break;
+
+        // Sort based on Title
+        if( this.props._sortBy === 'title' ) {
+            if ( this.props._order === 'asc' ) {
+                items.sort((A, B) => A.snippet.title.localeCompare(B.snippet.title) );
+            } else {
+                items.sort((A, B) => B.snippet.title.localeCompare(A.snippet.title) );
+            }
+        }
+
+        // Sort based on publishedDate
+        if( this.props._sortBy === 'publishedDate' ) {
+            if ( this.props._order === 'asc' ) {
+                items.sort((A, B) => moment.utc(A.snippet.publishedAt).diff(moment.utc(B.snippet.publishedAt)));
+            } else {
+                items.sort((A, B) => moment.utc(B.snippet.publishedAt).diff(moment.utc(A.snippet.publishedAt)));
+            }
         }
 
         return items.map(item => (
