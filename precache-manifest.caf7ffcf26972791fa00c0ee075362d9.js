7ccc76e3a7d6d5cfdaf90100350cff66716c191b
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-10-26 17:22:55 +0530

    Updates

---
+++ precache-manifest.caf7ffcf26972791fa00c0ee075362d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41ba3767178a60813e3b",
+    "url": "/youtube-api-demo/static/js/runtime~main.41ba3767.js"
+  },
+  {
+    "revision": "52a9bf91075bab550580",
+    "url": "/youtube-api-demo/static/js/main.52a9bf91.chunk.js"
+  },
+  {
+    "revision": "8d02891eb9b6b7cf6d6c",
+    "url": "/youtube-api-demo/static/js/1.8d02891e.chunk.js"
+  },
+  {
+    "revision": "52a9bf91075bab550580",
+    "url": "/youtube-api-demo/static/css/main.6d1f84a3.chunk.css"
+  },
+  {
+    "revision": "8d02891eb9b6b7cf6d6c",
+    "url": "/youtube-api-demo/static/css/1.2172c2c1.chunk.css"
+  },
+  {
+    "revision": "5821889aeb68bf0ce3734251e09b9150",
+    "url": "/youtube-api-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84b0261794844498c20e038efa542065.js"
+  "/youtube-api-demo/precache-manifest.caf7ffcf26972791fa00c0ee075362d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/youtube-api-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
