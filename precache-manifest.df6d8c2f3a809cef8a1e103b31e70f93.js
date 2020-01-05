66be951bf50d59f22efc189b9e449ffc624950bf
akshitkrnagpal <akshitkrnagpal@users.noreply.github.com>
2020-01-05 21:49:43 +0000

    Automated deployment: Sun Jan  5 21:49:43 UTC 2020 de429f9bde0f69aa859de4349506b732c07c9591

---
+++ precache-manifest.df6d8c2f3a809cef8a1e103b31e70f93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c1b177e1f9f6c4b7486d3eab3738af",
+    "url": "/youtube-api-demo/index.html"
+  },
+  {
+    "revision": "adeca63af0536ba7a4d7",
+    "url": "/youtube-api-demo/static/css/2.d5831cfa.chunk.css"
+  },
+  {
+    "revision": "d60986fc704ee0979132",
+    "url": "/youtube-api-demo/static/css/main.f88a2eef.chunk.css"
+  },
+  {
+    "revision": "adeca63af0536ba7a4d7",
+    "url": "/youtube-api-demo/static/js/2.17703ab8.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/youtube-api-demo/static/js/2.17703ab8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d60986fc704ee0979132",
+    "url": "/youtube-api-demo/static/js/main.ec27c76a.chunk.js"
+  },
+  {
+    "revision": "4aa3dad6c211f68a2e96",
+    "url": "/youtube-api-demo/static/js/runtime-main.99d6bc9c.js"
+  }
+]);
