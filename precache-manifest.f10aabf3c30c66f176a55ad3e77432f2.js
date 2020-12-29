e9754599d7ca0042b10520c3d450cf5cbcafdf10
akshitkrnagpal <akshitkrnagpal@users.noreply.github.com>
2020-12-29 03:23:05 +0000

    Automated deployment: Tue Dec 29 03:23:05 UTC 2020 f50d83e14e22380ba5dccb8936aefa34fecdf3b4

---
+++ precache-manifest.f10aabf3c30c66f176a55ad3e77432f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31effabf58bb7cee078464f04f34730f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7bde638a3a5e3abf6d6",
+    "url": "/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "7d41c9375488e62a7027",
+    "url": "/static/css/main.f88a2eef.chunk.css"
+  },
+  {
+    "revision": "f7bde638a3a5e3abf6d6",
+    "url": "/static/js/2.ace4d287.chunk.js"
+  },
+  {
+    "revision": "29a5ae03b8cbf730398dd7f6ba2f7468",
+    "url": "/static/js/2.ace4d287.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d41c9375488e62a7027",
+    "url": "/static/js/main.9982e165.chunk.js"
+  },
+  {
+    "revision": "4da87685091793bba942",
+    "url": "/static/js/runtime-main.1a6e896c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youtube-api-demo/precache-manifest.8cf30743dff037ea704f6115df706a9b.js"
+  "/precache-manifest.f10aabf3c30c66f176a55ad3e77432f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtube-api-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
